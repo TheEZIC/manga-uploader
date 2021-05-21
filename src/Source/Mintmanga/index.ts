ad8d3835d6a4a560f4ee624d5a6c778c0d7e10a2
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2021-05-21 12:40:09 +0300

    Create index.ts

---
+++ src/Source/Mintmanga/index.ts
@@ -0,0 +1,6 @@
+import ReadmangaSource from "../Readmanga";
+
+export default class MintmangaSource extends ReadmangaSource {
+    public name = "Mintmanga";
+    public baseLink = "https://mintmanga.live/";
+}
