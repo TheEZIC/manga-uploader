63b575f5036ee4be7a76dc1ed598785f7dbd44c9
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2021-05-21 12:54:37 +0300

    Update MangaUploader.ts

--- src/MangaUploader.ts
+++ src/MangaUploader.ts
@@ -1,11 +1,17 @@
-import { launch as startPuppeteer, Browser } from "puppeteer";
+import { Browser } from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
 import Logger from "./Logger";
-import MangalibSource from "./Source/Mangalib";
 import Source from "./Source/Source";
+import IAuthorizationData from "./IAuthorizationData";
+
+import MangalibSource from "./Source/Mangalib";
+import RemangaSource from "./Source/Remanga";
+import ReadmangaSource from "./Source/Readmanga";
+import MintmangaSource from "./Source/Mintmanga";
 
 import data from "../authorizationData.json";
-import IAuthorizationData from "./IAuthorizationData";
-import RemangaSource from "./Source/Remanga";
 
 let authorizationData = data as IAuthorizationData[];
 
@@ -13,28 +19,45 @@ export default class MangaUploader {
     private browser: Browser;
     private sources: Source[];
 
+    constructor() {
+        puppeteer.use(StealthPlugin());
+    }
+
     async launch() {
-        this.browser = await startPuppeteer({ headless: false });
+        this.browser = await puppeteer.launch({
+            //@ts-ignore
+            headless: false,
+            defaultViewport: {
+                width: 1280,
+                height: 720,
+            },
+        });
 
         this.sources = [
             new MangalibSource(this.browser),
+            new RemangaSource(this.browser),
+            new ReadmangaSource(this.browser),
+            new MintmangaSource(this.browser),
         ];
 
-        for await (let source of this.sources) {
+        for (let i = 0; i < this.sources.length; i++) {
+            const source = this.sources[i];
             await source.init();
+            await source.authorizer.authorize(authorizationData[i]);
         }
     }
 
     async test() {
-        this.browser = await startPuppeteer({
-            headless: false ,
+        this.browser = await puppeteer.launch({
+            //@ts-ignore
+            headless: false,
             defaultViewport: {
                 width: 1280,
                 height: 720,
-            }
+            },
         });
 
-        const source = new MangalibSource(this.browser);
+        const source = new MintmangaSource(this.browser);
         await source.init();
         await source.authorizer.authorize(authorizationData[0]);
 
